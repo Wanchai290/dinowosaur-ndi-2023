4ffcbbf9486a9e442266fd97078cadd617f53789
Mathis <ard.foussac@gmail.com>
2023-12-07 21:27:33 +0100

    feat(#14): add a card initialisation template in the tinder datas

---
+++ app/tinder/assets/data.tsx
@@ -0,0 +1,17 @@
+import { Card } from "../models/card.model"
+
+export const tinderDatas: Card[] = [
+    {
+        question: {
+            question: "aa",
+            answer: true,
+            imageLink: ""
+        },
+
+        answer: {
+            imageLink: "string",
+            text: "string",
+            link: "string"
+        }
+    },
+];
