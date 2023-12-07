17b71cd0fbad74aa164ed602102209f8d7c8ca18
Mathis <ard.foussac@gmail.com>
2023-12-07 22:43:20 +0100

    feat(#14): add the dataset of the tinder game

--- app/tinder/assets/data.tsx
+++ app/tinder/assets/data.tsx
@@ -2,16 +2,363 @@ import { Card } from "../models/card.model"
 
 export const tinderDatas: Card[] = [
   {
-	  question: {
-		  question: "",
-		  answer: true,
-		  imageLink: ""
-  	},
-
-	  answer: {
-			  imageLink: "",
-			  text: "",
-		    link: ""
-	  }
+    question: {
+      question: "Le réchauffement climatique n'est pas réel, c'est juste une théorie alarmiste.",
+      answer: false,
+      imageLink: "../assets/question_1.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_1.png",
+      text: "Faux. Le réchauffement climatique est bien réel. Selon le GIEC, la température mondiale a augmenté d'environ 1,1 °C depuis l'ère préindustrielle.",
+      link: "https://www.ipcc.ch/"
+    }
+  },
+  {
+    question: {
+      question: "Les énergies renouvelables ne représentent qu'une petite partie de la production mondiale d'électricité.",
+      answer: false,
+      imageLink: "../assets/question_2.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_2.png",
+      text: "Faux. Les énergies renouvelables ont atteint près de 28 % de la production mondiale d'électricité en 2020, selon l'Agence Internationale de l'Énergie (AIE).",
+      link: "https://www.iea.org/"
+    }
+  },
+  {
+    question: {
+      question: "Le tri des déchets ne sert à rien, tout finit dans la même poubelle.",
+      answer: false,
+      imageLink: "../assets/question_3.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_3.png",
+      text: "Faux. Le tri des déchets est essentiel. En France, près de 67 % des déchets sont recyclés ou valorisés.",
+      link: "https://www.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "La déforestation n'a pas d'impact significatif sur le changement climatique.",
+      answer: false,
+      imageLink: "../assets/question_4.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_4.png",
+      text: "Faux. La déforestation contribue fortement aux émissions de gaz à effet de serre. Elle représente environ 15 % des émissions mondiales.",
+      link: "https://reseauactionclimat.org/urgence-climatique/"
+    }
+  },
+  {
+    question: {
+      question: "Les voitures électriques sont plus polluantes que les voitures à essence.",
+      answer: false,
+      imageLink: "../assets/question_5.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_5.png",
+      text: "Faux. Sur l'ensemble de leur cycle de vie, les voitures électriques émettent moins de gaz à effet de serre que les voitures à essence, selon l'ADEME.",
+      link: "https://www.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "La transition énergétique coûte trop cher et n'est pas rentable.",
+      answer: false,
+      imageLink: "../assets/question_6.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_6.png",
+      text: "Faux. Selon l'ADEME, la transition énergétique pourrait générer près de 900 000 emplois nets en France d'ici 2050.",
+      link: "https://data.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "L'acidification des océans n'affecte que les animaux marins, pas les humains.",
+      answer: false,
+      imageLink: "../assets/question_7.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_7.png",
+      text: "Faux. L'acidification des océans menace la sécurité alimentaire humaine en affectant les poissons et les organismes marins que nous consommons.",
+      link: "https://www.hautconseilclimat.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Les forêts plantées sont aussi bénéfiques que les forêts naturelles pour le climat.",
+      answer: false,
+      imageLink: "../assets/question_8.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_8.png",
+      text: "Faux. Les forêts naturelles stockent plus de carbone et sont plus riches en biodiversité que les forêts plantées, selon le Haut Conseil pour le Climat.",
+      link: "https://www.hautconseilclimat.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Les actions individuelles ne peuvent pas faire une réelle différence dans la lutte contre le changement climatique.",
+      answer: false,
+      imageLink: "../assets/question_9.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_9.png",
+      text: "Faux. Chacun de nous peut contribuer. Des gestes simples, comme réduire sa consommation d'eau et d'électricité, ont un impact positif sur l'environnement.",
+      link: "https://nosgestesclimat.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Les technologies de capture et de stockage du carbone sont la solution miracle pour contrer le changement climatique.",
+      answer: false,
+      imageLink: "../assets/question_10.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_10.png",
+      text: "Faux. Bien que prometteuses, les technologies de capture du carbone ne peuvent pas remplacer une réduction significative des émissions de CO2, selon le GIEC.",
+      link: "https://www.ipcc.ch/"
+    }
+  },
+  {
+    question: {
+      question: "La fonte des glaces n'affecte que les régions polaires, pas le reste du monde.",
+      answer: false,
+      imageLink: "../assets/question_11.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_11.png",
+      text: "Faux. La fonte des glaces contribue à l'élévation du niveau de la mer, menaçant les zones côtières du monde entier.",
+      link: "https://www.ipcc.ch/"
+    }
+  },
+  {
+    question: {
+      question: "Les émissions de méthane n'ont pas d'impact significatif sur le climat.",
+      answer: false,
+      imageLink: "../assets/question_12.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_12.png",
+      text: "Faux. Le méthane est un gaz à effet de serre puissant. Bien que présent en quantité moindre que le CO2, il contribue de manière significative au réchauffement climatique.",
+      link: "https://www.ipcc.ch/"
+    }
+  },
+  {
+    question: {
+      question: "L'agriculture biologique est moins productive et ne peut pas nourrir la population mondiale.",
+      answer: false,
+      imageLink: "../assets/question_13.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_13.png",
+      text: "Faux. Selon l'ADEME, l'agriculture biologique peut être aussi productive que l'agriculture conventionnelle, tout en préservant les sols et la biodiversité.",
+      link: "https://www.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "La hausse de la température mondiale n'est pas liée aux activités humaines.",
+      answer: false,
+      imageLink: "../assets/question_14.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_14.png",
+      text: "Faux. Selon le GIEC, il est très probable que plus de la moitié de l'augmentation de la température depuis 1950 soit due aux activités humaines.",
+      link: "https://www.ipcc.ch/"
+    }
+  },
+  {
+    question: {
+      question: "Les océans absorbent plus de CO2 que les forêts, donc il est inutile de préserver les forêts.",
+      answer: false,
+      imageLink: "../assets/question_15.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_15.png",
+      text: "Faux. Les forêts jouent un rôle crucial dans le stockage du carbone et la préservation de la biodiversité. Préserver les forêts est essentiel.",
+      link: "https://reseauactionclimat.org/urgence-climatique/"
+    }
+  },
+  {
+    question: {
+      question: "Les technologies vertes ne sont pas encore assez avancées pour faire une réelle différence.",
+      answer: false,
+      imageLink: "../assets/question_16.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_16.png",
+      text: "Faux. De nombreuses technologies vertes existent et peuvent contribuer à réduire les émissions de gaz à effet de serre. L'innovation est en marche.",
+      link: "https://data-transitions2050.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Les tempêtes et les phénomènes météorologiques extrêmes ne sont pas liés au changement climatique.",
+      answer: false,
+      imageLink: "../assets/question_17.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_17.png",
+      text: "Faux. Selon le GIEC, il y a une forte corrélation entre l'augmentation des phénomènes extrêmes et le changement climatique.",
+      link: "https://www.ipcc.ch/"
+    }
+  },
+  {
+    question: {
+      question: "La consommation de viande n'a aucun impact sur le changement climatique.",
+      answer: false,
+      imageLink: "../assets/question_18.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_18.png",
+      text: "Faux. L'industrie de la viande est une source importante d'émissions de gaz à effet de serre. Réduire la consommation de viande peut contribuer à la lutte contre le changement climatique.",
+      link: "https://www.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Le nucléaire est la seule solution viable pour produire de l'énergie sans émettre de gaz à effet de serre.",
+      answer: false,
+      imageLink: "../assets/question_19.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_19.png",
+      text: "Faux. Bien que le nucléaire ne produise pas de CO2 pendant son fonctionnement, il pose des défis en termes de sécurité, de déchets radioactifs et de coûts.",
+      link: "https://data.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Le changement climatique n'affecte que la nature, pas les sociétés humaines.",
+      answer: false,
+      imageLink: "../assets/question_20.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_20.png",
+      text: "Faux. Le changement climatique a des impacts graves sur les sociétés humaines, provoquant des phénomènes météorologiques extrêmes, des migrations forcées et des risques pour la sécurité alimentaire.",
+      link: "https://reseauactionclimat.org/urgence-climatique/"
+    }
+  },
+  {
+    question: {
+      question: "Les pets de vache contribuent-ils au réchauffement climatique ?",
+      answer: true,
+      imageLink: "../assets/question_21.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_21.png",
+      text: "Vrai. Les pets de vache émettent du méthane, un gaz à effet de serre puissant. Réduire la consommation de viande peut contribuer à la lutte contre le changement climatique.",
+      link: "https://www.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Planter un arbre par jour peut-il sauver la planète ?",
+      answer: true,
+      imageLink: "../assets/question_22.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_22.png",
+      text: "Vrai. Les arbres absorbent le dioxyde de carbone (CO2) de l'air. Planter des arbres peut aider à compenser les émissions de gaz à effet de serre.",
+      link: "https://reseauactionclimat.org/urgence-climatique/"
+    }
+  },
+  {
+    question: {
+      question: "Le recyclage du plastique transforme-t-il les déchets en licornes ?",
+      answer: false,
+      imageLink: "../assets/question_23.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_23.png",
+      text: "Faux. Le recyclage du plastique est important, mais il ne transforme malheureusement pas les déchets en licornes. Il contribue cependant à réduire la pollution.",
+      link: "https://www.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Le soleil envoie-t-il des factures pour son énergie solaire ?",
+      answer: false,
+      imageLink: "../assets/question_24.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_24.png",
+      text: "Faux. Le soleil fournit son énergie gratuitement. Les panneaux solaires permettent de capturer cette énergie pour produire de l'électricité.",
+      link: "https://www.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Les glaciers fondent-ils à cause des glaçons oubliés dans les boissons ?",
+      answer: false,
+      imageLink: "../assets/question_25.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_25.png",
+      text: "Faux. Les glaciers fondent en raison du réchauffement climatique dû aux activités humaines, pas à cause des glaçons oubliés dans les boissons.",
+      link: "https://www.ipcc.ch/"
+    }
+  },
+  {
+    question: {
+      question: "Les éoliennes sont-elles secrètement des ventilateurs géants pour rafraîchir la Terre ?",
+      answer: false,
+      imageLink: "../assets/question_26.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_26.png",
+      text: "Faux. Les éoliennes produisent de l'électricité en utilisant la force du vent, mais elles ne sont pas des ventilateurs géants pour rafraîchir la Terre.",
+      link: "https://data-transitions2050.ademe.fr/"
+    }
+  },
+  {
+    question: {
+      question: "Les ours polaires pratiquent-ils le yoga pour s'adapter au changement climatique ?",
+      answer: false,
+      imageLink: "../assets/question_27.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_27.png",
+      text: "Faux. Les ours polaires font face à de réelles menaces en raison de la fonte des glaces, mais ils ne pratiquent pas le yoga pour s'adapter.",
+      link: "https://www.ipcc.ch/"
+    }
+  },
+  {
+    question: {
+      question: "Les gouttes de pluie sont-elles des larmes de tristesse de la planète ?",
+      answer: false,
+      imageLink: "../assets/question_28.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_28.png",
+      text: "Faux. Les gouttes de pluie sont le résultat du cycle de l'eau sur la Terre. Elles ne sont pas des larmes de tristesse, mais essentielles pour la vie.",
+      link: "https://climat.cned.fr/formations/"
+    }
+  },
+  {
+    question: {
+      question: "Les océans ont-ils un bouton pour régler leur niveau de montée ?",
+      answer: false,
+      imageLink: "../assets/question_29.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_29.png",
+      text: "Faux. La montée du niveau de la mer est due à la fonte des glaces et à l'expansion thermique de l'eau, pas à un bouton de réglage dans les océans.",
+      link: "https://www.ipcc.ch/"
+    }
+  },
+  {
+    question: {
+      question: "Les arbres se plaignent-ils de la déforestation sur les réseaux sociaux ?",
+      answer: false,
+      imageLink: "../assets/question_30.png"
+    },
+    answer: {
+      imageLink: "../assets/answer_30.png",
+      text: "Faux. Les arbres ne se plaignent pas sur les réseaux sociaux, mais la déforestation a un impact grave sur l'équilibre écologique et le climat.",
+      link: "https://reseauactionclimat.org/urgence-climatique/"
+    }
   }
 ];
