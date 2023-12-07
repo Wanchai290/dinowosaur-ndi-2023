c1c4ebde48d2d2704d18791445c69394a26ffc6b
Mathis <ard.foussac@gmail.com>
2023-12-07 22:00:34 +0100

    chore: fix lint

--- app/tinder/assets/data.tsx
+++ app/tinder/assets/data.tsx
@@ -1,17 +1,17 @@
 import { Card } from "../models/card.model"
 
 export const tinderDatas: Card[] = [
-    {
-        question: {
-            question: "aa",
-            answer: true,
-            imageLink: ""
-        },
+  {
+	  question: {
+		  question: "",
+		  answer: true,
+		  imageLink: ""
+  	},
 
-        answer: {
-            imageLink: "string",
-            text: "string",
-            link: "string"
-        }
-    },
+	  answer: {
+			  imageLink: "",
+			  text: "",
+		    link: ""
+	  }
+  }
 ];
--- app/tinder/components/tinderApi.tsx
+++ app/tinder/components/tinderApi.tsx
@@ -6,12 +6,11 @@ import {tinderDatas} from "../assets/data"
 export default function TinderApi() {
 
 
-    function getAllCards(): Card[] {
-        return tinderDatas;
-    }
-
-    function getNCards(numberCards: number): Card[] {
-        
-        return numberCards > 0 ? [...tinderDatas].sort(() => 0.5 - Math.random()).slice(0, numberCards) : [];
-    }
+  function getAllCards(): Card[] {
+    return tinderDatas;
   }
+
+  function getNCards(numberCards: number): Card[] {
+  	return numberCards > 0 ? [...tinderDatas].sort(() => 0.5 - Math.random()).slice(0, numberCards) : [];
+  }
+}
