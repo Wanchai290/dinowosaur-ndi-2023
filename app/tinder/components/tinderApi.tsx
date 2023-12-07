901c4aaaf071703683a40768abd7e71569abfc4c
Mathis <ard.foussac@gmail.com>
2023-12-07 23:53:37 +0100

    feat(#24): add the tinder game service

--- app/tinder/components/tinderApi.tsx
+++ app/tinder/components/tinderApi.tsx
@@ -2,15 +2,36 @@ import { CardModel } from "../models/card.model";
 
 import {tinderDatas} from "../assets/data"
 
+interface GameModel {
+  leftCards: CardModel[];
+  score: number;
+  currentCard: CardModel;
+}
 
-export default function TinderApi() {
+let currentGame:  GameModel;
 
+const defaultCardNumber = 6;
 
-  function getAllCards(): CardModel[] {
-    return tinderDatas;
+export function startGame(nbCards: number) {
+  nbCards = nbCards > 0 ? nbCards : defaultCardNumber;
+  let cards = getNCards(nbCards);
+  let currentCard = cards.shift() as CardModel;
+  currentGame = {leftCards: cards, score: 0, currentCard: currentCard};
+
+}
+
+export function nextCard(): CardModel | undefined {
+  let nextCard = currentGame.leftCards.shift();
+  if (nextCard !== undefined) {
+    currentGame.currentCard = nextCard;
   }
+  return nextCard;
+}
 
-  function getNCards(numberCards: number): CardModel[] {
-  	return numberCards > 0 ? [...tinderDatas].sort(() => 0.5 - Math.random()).slice(0, numberCards) : [];
-  }
+export function setUserResponse(response: boolean): number {
+  return (response == currentGame.currentCard.question.answer) ? currentGame.score += 1 : currentGame.score;
+}
+
+function getNCards(numberCards: number): CardModel[] {
+  return numberCards > 0 ? [...tinderDatas].sort(() => 0.5 - Math.random()).slice(0, numberCards) : [];
 }
