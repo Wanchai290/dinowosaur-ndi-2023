e14686cc41b1ce72f6bb39e59ff6bcc3f53db4a5
tfourcaudot <tom.fourcaudot@gmail.com>
2023-12-08 08:01:22 +0100

    FEAT: Add title debate

--- app/debate/page.tsx
+++ app/debate/page.tsx
@@ -17,7 +17,12 @@ export default function Home() {
 
     return (
         <main className="flex min-h-screen max-h-screen min-w-full flex-col items-center justify-between">
-            <canvas id="game-canvas" ref={canvasRef} />
+            <h1
+            className="py-10 text-center text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span
+            className="text-transparent bg-clip-text bg-gradient-to-r to-yellow-200 from-pink-500">Debat </span>climatique.
+            </h1>
+            <p className="text-center text-xl font-bold pb-4">{"Combat les stéréotypes climatiques dans un tribunal !"}</p>
+            <canvas id="game-canvas" ref={canvasRef}/>
         </main>
     );
 }
