99235922e1a9a344b9720e84b44db273f11a060a
BOUGET Alexandre <abouget68@gmail.com>
2023-12-08 02:24:16 +0100

    feat: continue engine

--- app/debate/components/gameEngine.tsx
+++ app/debate/components/gameEngine.tsx
@@ -6,21 +6,21 @@ export default function debate(canvasref: HTMLCanvasElement)
 {
     const assetRoot = "debate/assets/"
     const game = kaboom({
-        width: canvasref.parentElement?.clientWidth - 60,
-        height: canvasref.parentElement?.clientHeight,
+        width: canvasref.parentElement?.clientWidth,
+        height: canvasref.parentElement?.clientHeight - 60,
         // if you don't want to import to the global namespace
         global: false,
         // if you don't want kaboom to create a canvas and insert under document.body
         canvas: canvasref,
     });
 
-    game.loadSprite("back" , assetRoot + "Courtroom.jpg");
-
+    const back = game.loadSprite("back" , assetRoot + "Courtroom.jpg");
+    
     game.add([
         game.sprite("back"),
         game.pos(game.width() / 2, game.height() / 2),
         game.anchor("center"),
-        game.scale(2),
+        game.scale(game.width(), game.height()),
     ]);
 
     game.loop(1, () => {
