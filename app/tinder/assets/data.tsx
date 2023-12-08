f8fafc639b1e20056797233e1731e337f4dce6a6
Mathis <ard.foussac@gmail.com>
2023-12-08 03:34:01 +0100

    feat: change images called during rendering of the game

--- app/tinder/assets/data.tsx
+++ app/tinder/assets/data.tsx
@@ -6,10 +6,10 @@ export const tinderDatas: CardModel[] = [
       title: "Réchauffement climatique",
       question: "Le réchauffement climatique n'est pas réel, c'est juste une théorie alarmiste.",
       answer: false,
-      imageLink: "../assets/question_1.png"
+      imageLink: "/games/tinder/images//image_1.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_1.png",
+      imageLink: "/games/tinder/images//image_1.jpg",
       text: "Faux. Le réchauffement climatique est bien réel. Selon le GIEC, la température mondiale a augmenté d'environ 1,1 °C depuis l'ère préindustrielle.",
       link: "https://www.ipcc.ch/"
     }
@@ -19,10 +19,10 @@ export const tinderDatas: CardModel[] = [
       title: "Énergies renouvelables",
       question: "Les énergies renouvelables ne représentent qu'une petite partie de la production mondiale d'électricité.",
       answer: false,
-      imageLink: "../assets/question_2.png"
+      imageLink: "/games/tinder/images//image_2.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_2.png",
+      imageLink: "/games/tinder/images//image_2.jpg",
       text: "Faux. Les énergies renouvelables ont atteint près de 28 % de la production mondiale d'électricité en 2020, selon l'Agence Internationale de l'Énergie (AIE).",
       link: "https://www.iea.org/"
     }
@@ -32,10 +32,10 @@ export const tinderDatas: CardModel[] = [
       title: "Tri des déchets",
       question: "Le tri des déchets ne sert à rien, tout finit dans la même poubelle.",
       answer: false,
-      imageLink: "../assets/question_3.png"
+      imageLink: "/games/tinder/images//image_3.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_3.png",
+      imageLink: "/games/tinder/images//image_3.jpg",
       text: "Faux. Le tri des déchets est essentiel. En France, près de 67 % des déchets sont recyclés ou valorisés.",
       link: "https://www.ademe.fr/"
     }
@@ -45,10 +45,10 @@ export const tinderDatas: CardModel[] = [
       title: "Déforestation",
       question: "La déforestation n'a pas d'impact significatif sur le changement climatique.",
       answer: false,
-      imageLink: "../assets/question_4.png"
+      imageLink: "/games/tinder/images//image_4.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_4.png",
+      imageLink: "/games/tinder/images//image_4.jpg",
       text: "Faux. La déforestation contribue fortement aux émissions de gaz à effet de serre. Elle représente environ 15 % des émissions mondiales.",
       link: "https://reseauactionclimat.org/urgence-climatique/"
     }
@@ -58,10 +58,10 @@ export const tinderDatas: CardModel[] = [
       title: "Voitures électriques",
       question: "Les voitures électriques sont plus polluantes que les voitures à essence.",
       answer: false,
-      imageLink: "../assets/question_5.png"
+      imageLink: "/games/tinder/images//image_5.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_5.png",
+      imageLink: "/games/tinder/images//image_5.jpg",
       text: "Faux. Sur l'ensemble de leur cycle de vie, les voitures électriques émettent moins de gaz à effet de serre que les voitures à essence, selon l'ADEME.",
       link: "https://www.ademe.fr/"
     }
@@ -71,10 +71,10 @@ export const tinderDatas: CardModel[] = [
       title: "Transition énergétique",
       question: "La transition énergétique coûte trop cher et n'est pas rentable.",
       answer: false,
-      imageLink: "../assets/question_6.png"
+      imageLink: "/games/tinder/images//image_6.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_6.png",
+      imageLink: "/games/tinder/images//image_6.jpg",
       text: "Faux. Selon l'ADEME, la transition énergétique pourrait générer près de 900 000 emplois nets en France d'ici 2050.",
       link: "https://data.ademe.fr/"
     }
@@ -84,10 +84,10 @@ export const tinderDatas: CardModel[] = [
       title: "Acidification des océans",
       question: "L'acidification des océans n'affecte que les animaux marins, pas les humains.",
       answer: false,
-      imageLink: "../assets/question_7.png"
+      imageLink: "/games/tinder/images//image_7.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_7.png",
+      imageLink: "/games/tinder/images//image_7.jpg",
       text: "Faux. L'acidification des océans menace la sécurité alimentaire humaine en affectant les poissons et les organismes marins que nous consommons.",
       link: "https://www.hautconseilclimat.fr/"
     }
@@ -97,10 +97,10 @@ export const tinderDatas: CardModel[] = [
       title: "Forêts plantées",
       question: "Les forêts plantées sont aussi bénéfiques que les forêts naturelles pour le climat.",
       answer: false,
-      imageLink: "../assets/question_8.png"
+      imageLink: "/games/tinder/images//image_8.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_8.png",
+      imageLink: "/games/tinder/images//image_8.jpg",
       text: "Faux. Les forêts naturelles stockent plus de carbone et sont plus riches en biodiversité que les forêts plantées, selon le Haut Conseil pour le Climat.",
       link: "https://www.hautconseilclimat.fr/"
     }
@@ -110,10 +110,10 @@ export const tinderDatas: CardModel[] = [
       title: "Actions individuelles",
       question: "Les actions individuelles ne peuvent pas faire une réelle différence dans la lutte contre le changement climatique.",
       answer: false,
-      imageLink: "../assets/question_9.png"
+      imageLink: "/games/tinder/images//image_9.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_9.png",
+      imageLink: "/games/tinder/images//image_9.jpg",
       text: "Faux. Chacun de nous peut contribuer. Des gestes simples, comme réduire sa consommation d'eau et d'électricité, ont un impact positif sur l'environnement.",
       link: "https://nosgestesclimat.fr/"
     }
@@ -123,10 +123,10 @@ export const tinderDatas: CardModel[] = [
       title: "Capture et stockage du carbone",
       question: "Les technologies de capture et de stockage du carbone sont la solution miracle pour contrer le changement climatique.",
       answer: false,
-      imageLink: "../assets/question_10.png"
+      imageLink: "/games/tinder/images//image_0.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_10.png",
+      imageLink: "/games/tinder/images//image_0.jpg",
       text: "Faux. Bien que prometteuses, les technologies de capture du carbone ne peuvent pas remplacer une réduction significative des émissions de CO2, selon le GIEC.",
       link: "https://www.ipcc.ch/"
     }
@@ -136,10 +136,10 @@ export const tinderDatas: CardModel[] = [
       title: "Émissions de méthane",
       question: "Les émissions de méthane n'ont pas d'impact significatif sur le climat.",
       answer: false,
-      imageLink: "../assets/question_11.png"
+      imageLink: "/games/tinder/images//image_1.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_11.png",
+      imageLink: "/games/tinder/images//image_1.jpg",
       text: "Faux. Le méthane est un gaz à effet de serre puissant. Bien que présent en quantité moindre que le CO2, il contribue de manière significative au réchauffement climatique.",
       link: "https://www.ipcc.ch/"
     }
@@ -149,10 +149,10 @@ export const tinderDatas: CardModel[] = [
       title: "Agriculture biologique",
       question: "L'agriculture biologique est moins productive et ne peut pas nourrir la population mondiale.",
       answer: false,
-      imageLink: "../assets/question_12.png"
+      imageLink: "/games/tinder/images//image_2.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_12.png",
+      imageLink: "/games/tinder/images//image_2.jpg",
       text: "Faux. Selon l'ADEME, l'agriculture biologique peut être aussi productive que l'agriculture conventionnelle, tout en préservant les sols et la biodiversité.",
       link: "https://www.ademe.fr/"
     }
@@ -162,10 +162,10 @@ export const tinderDatas: CardModel[] = [
       title: "Température mondiale",
       question: "La hausse de la température mondiale n'est pas liée aux activités humaines.",
       answer: false,
-      imageLink: "../assets/question_13.png"
+      imageLink: "/games/tinder/images//image_3.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_13.png",
+      imageLink: "/games/tinder/images//image_3.jpg",
       text: "Faux. Selon le GIEC, il est très probable que plus de la moitié de l'augmentation de la température depuis 1950 soit due aux activités humaines.",
       link: "https://www.ipcc.ch/"
     }
@@ -175,10 +175,10 @@ export const tinderDatas: CardModel[] = [
       title: "Absorption de CO2",
       question: "Les océans absorbent plus de CO2 que les forêts, donc il est inutile de préserver les forêts.",
       answer: false,
-      imageLink: "../assets/question_14.png"
+      imageLink: "/games/tinder/images//image_4.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_14.png",
+      imageLink: "/games/tinder/images//image_4.jpg",
       text: "Faux. Les forêts jouent un rôle crucial dans le stockage du carbone et la préservation de la biodiversité. Préserver les forêts est essentiel.",
       link: "https://reseauactionclimat.org/urgence-climatique/"
     }
@@ -188,10 +188,10 @@ export const tinderDatas: CardModel[] = [
       title: "Technologies vertes",
       question: "Les technologies vertes ne sont pas encore assez avancées pour faire une réelle différence.",
       answer: false,
-      imageLink: "../assets/question_15.png"
+      imageLink: "/games/tinder/images//image_5.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_15.png",
+      imageLink: "/games/tinder/images//image_5.jpg",
       text: "Faux. De nombreuses technologies vertes existent et peuvent contribuer à réduire les émissions de gaz à effet de serre. L'innovation est en marche.",
       link: "https://data-transitions2050.ademe.fr/"
     }
@@ -201,10 +201,10 @@ export const tinderDatas: CardModel[] = [
       title: "Phénomènes météorologiques extrêmes",
       question: "Les tempêtes et les phénomènes météorologiques extrêmes ne sont pas liés au changement climatique.",
       answer: false,
-      imageLink: "../assets/question_16.png"
+      imageLink: "/games/tinder/images//image_6.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_16.png",
+      imageLink: "/games/tinder/images//image_6.jpg",
       text: "Faux. Selon le GIEC, il y a une forte corrélation entre l'augmentation des phénomènes extrêmes et le changement climatique.",
       link: "https://www.ipcc.ch/"
     }
@@ -214,10 +214,10 @@ export const tinderDatas: CardModel[] = [
       title: "Consommation de viande",
       question: "La consommation de viande n'a aucun impact sur le changement climatique.",
       answer: false,
-      imageLink: "../assets/question_17.png"
+      imageLink: "/games/tinder/images//image_7.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_17.png",
+      imageLink: "/games/tinder/images//image_7.jpg",
       text: "Faux. L'industrie de la viande est une source importante d'émissions de gaz à effet de serre. Réduire la consommation de viande peut contribuer à la lutte contre le changement climatique.",
       link: "https://www.ademe.fr/"
     }
@@ -227,10 +227,10 @@ export const tinderDatas: CardModel[] = [
       title: "Énergie nucléaire",
       question: "Le nucléaire est la seule solution viable pour produire de l'énergie sans émettre de gaz à effet de serre.",
       answer: false,
-      imageLink: "../assets/question_18.png"
+      imageLink: "/games/tinder/images//image_8.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_18.png",
+      imageLink: "/games/tinder/images//image_8.jpg",
       text: "Faux. Bien que le nucléaire ne produise pas de CO2 pendant son fonctionnement, il pose des défis en termes de sécurité, de déchets radioactifs et de coûts.",
       link: "https://data.ademe.fr/"
     }
@@ -240,10 +240,10 @@ export const tinderDatas: CardModel[] = [
       title: "Impact sur les sociétés humaines",
       question: "Le changement climatique n'affecte que la nature, pas les sociétés humaines.",
       answer: false,
-      imageLink: "../assets/question_19.png"
+      imageLink: "/games/tinder/images//image_9.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_19.png",
+      imageLink: "/games/tinder/images//image_9.jpg",
       text: "Faux. Le changement climatique a des impacts graves sur les sociétés humaines, provoquant des phénomènes météorologiques extrêmes, des migrations forcées et des risques pour la sécurité alimentaire.",
       link: "https://reseauactionclimat.org/urgence-climatique/"
     }
@@ -253,10 +253,10 @@ export const tinderDatas: CardModel[] = [
       title: "Migrations forcées",
       question: "Les changements climatiques provoquent-ils des migrations forcées de pingouins ?",
       answer: false,
-      imageLink: "../assets/question_20.png"
+      imageLink: "/games/tinder/images//image_0.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_20.png",
+      imageLink: "/games/tinder/images//image_0.jpg",
       text: "Faux. Les pingouins ne migrent pas en raison du changement climatique. Cependant, les changements climatiques affectent certaines espèces animales et provoquent des migrations.",
       link: "https://reseauactionclimat.org/urgence-climatique/"
     }
@@ -266,10 +266,10 @@ export const tinderDatas: CardModel[] = [
       title: "Méthane animal",
       question: "Les pets de vache contribuent-ils au réchauffement climatique ?",
       answer: true,
-      imageLink: "../assets/question_21.png"
+      imageLink: "/games/tinder/images//image_1.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_21.png",
+      imageLink: "/games/tinder/images//image_1.jpg",
       text: "Vrai. Les pets de vache émettent du méthane, un gaz à effet de serre puissant. Réduire la consommation de viande peut contribuer à la lutte contre le changement climatique.",
       link: "https://www.ademe.fr/"
     }
@@ -279,10 +279,10 @@ export const tinderDatas: CardModel[] = [
       title: "Sauver la planète",
       question: "Planter un arbre par jour peut-il sauver la planète ?",
       answer: true,
-      imageLink: "../assets/question_22.png"
+      imageLink: "/games/tinder/images//image_2.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_22.png",
+      imageLink: "/games/tinder/images//image_2.jpg",
       text: "Vrai. Les arbres absorbent le dioxyde de carbone (CO2) de l'air. Planter des arbres peut aider à compenser les émissions de gaz à effet de serre.",
       link: "https://reseauactionclimat.org/urgence-climatique/"
     }
@@ -292,10 +292,10 @@ export const tinderDatas: CardModel[] = [
       title: "Recyclage magique",
       question: "Le recyclage du plastique transforme-t-il les déchets en licornes ?",
       answer: false,
-      imageLink: "../assets/question_23.png"
+      imageLink: "/games/tinder/images//image_3.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_23.png",
+      imageLink: "/games/tinder/images//image_3.jpg",
       text: "Faux. Le recyclage du plastique est important, mais il ne transforme malheureusement pas les déchets en licornes. Il contribue cependant à réduire la pollution.",
       link: "https://www.ademe.fr/"
     }
@@ -305,10 +305,10 @@ export const tinderDatas: CardModel[] = [
       title: "Factures solaires",
       question: "Le soleil envoie-t-il des factures pour son énergie solaire ?",
       answer: false,
-      imageLink: "../assets/question_24.png"
+      imageLink: "/games/tinder/images//image_4.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_24.png",
+      imageLink: "/games/tinder/images//image_4.jpg",
       text: "Faux. Le soleil fournit son énergie gratuitement. Les panneaux solaires permettent de capturer cette énergie pour produire de l'électricité.",
       link: "https://www.ademe.fr/"
     }
@@ -318,10 +318,10 @@ export const tinderDatas: CardModel[] = [
       title: "Glaçons coupables",
       question: "Les glaciers fondent-ils à cause des glaçons oubliés dans les boissons ?",
       answer: false,
-      imageLink: "../assets/question_25.png"
+      imageLink: "/games/tinder/images//image_5.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_25.png",
+      imageLink: "/games/tinder/images//image_5.jpg",
       text: "Faux. Les glaciers fondent en raison du réchauffement climatique dû aux activités humaines, pas à cause des glaçons oubliés dans les boissons.",
       link: "https://www.ipcc.ch/"
     }
@@ -331,10 +331,10 @@ export const tinderDatas: CardModel[] = [
       title: "Ventilateurs géants",
       question: "Les éoliennes sont-elles secrètement des ventilateurs géants pour rafraîchir la Terre ?",
       answer: false,
-      imageLink: "../assets/question_26.png"
+      imageLink: "/games/tinder/images//image_6.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_26.png",
+      imageLink: "/games/tinder/images//image_6.jpg",
       text: "Faux. Les éoliennes produisent de l'électricité en utilisant la force du vent, mais elles ne sont pas des ventilateurs géants pour rafraîchir la Terre.",
       link: "https://data-transitions2050.ademe.fr/"
     }
@@ -344,10 +344,10 @@ export const tinderDatas: CardModel[] = [
       title: "Yoga polaire",
       question: "Les ours polaires pratiquent-ils le yoga pour s'adapter au changement climatique ?",
       answer: false,
-      imageLink: "../assets/question_27.png"
+      imageLink: "/assets/image_7.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_27.png",
+      imageLink: "/games/tinder/images//image_7.jpg",
       text: "Faux. Les ours polaires font face à de réelles menaces en raison de la fonte des glaces, mais ils ne pratiquent pas le yoga pour s'adapter.",
       link: "https://www.ipcc.ch/"
     }
@@ -357,10 +357,10 @@ export const tinderDatas: CardModel[] = [
       title: "Larmes de pluie",
       question: "Les gouttes de pluie sont-elles des larmes de tristesse de la planète ?",
       answer: false,
-      imageLink: "../assets/question_28.png"
+      imageLink: "/games/tinder/images//image_8.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_28.png",
+      imageLink: "/games/tinder/images//image_8.jpg",
       text: "Faux. Les gouttes de pluie sont le résultat du cycle de l'eau sur la Terre. Elles ne sont pas des larmes de tristesse, mais essentielles pour la vie.",
       link: "https://climat.cned.fr/formations/"
     }
@@ -370,10 +370,10 @@ export const tinderDatas: CardModel[] = [
       title: "Bouton océanique",
       question: "Les océans ont-ils un bouton pour régler leur niveau de montée ?",
       answer: false,
-      imageLink: "../assets/question_29.png"
+      imageLink: "/games/tinder/images//image_9.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_29.png",
+      imageLink: "/games/tinder/images//image_9.jpg",
       text: "Faux. La montée du niveau de la mer est due à la fonte des glaces et à l'expansion thermique de l'eau, pas à un bouton de réglage dans les océans.",
       link: "https://www.ipcc.ch/"
     }
@@ -383,10 +383,10 @@ export const tinderDatas: CardModel[] = [
       title: "Plaintes arboricoles",
       question: "Les arbres se plaignent-ils de la déforestation sur les réseaux sociaux ?",
       answer: false,
-      imageLink: "../assets/question_30.png"
+      imageLink: "/games/tinder/images//image_0.jpg"
     },
     answer: {
-      imageLink: "../assets/answer_30.png",
+      imageLink: "/games/tinder/images//image_0.jpg",
       text: "Faux. Les arbres ne se plaignent pas sur les réseaux sociaux, mais la déforestation a un impact grave sur l'équilibre écologique et le climat.",
       link: "https://reseauactionclimat.org/urgence-climatique/"
     }
