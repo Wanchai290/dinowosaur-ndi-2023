63a1073d14d012cf09d17570bb68e93053eecb07
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2023-12-08 07:41:18 +0100

    fix: color in dark mode

--- app/rpg_game/ControlsDisplay.tsx
+++ app/rpg_game/ControlsDisplay.tsx
@@ -1,7 +1,7 @@
 export default function ControlsDisplay() {
     return (
         <div style={{left: 54 + "rem"}}
-             className="text-right absolute w-[50%] h-[100%] bg-[#efe3fa]">
+             className="text-right absolute w-[50%] h-[100%] bg-[#efe3fa] dark:bg-purple-500">
             <h1 className="text-5xl mr-10">Contrôles</h1>
             <div className="p-4 mt-2.5 mr-20 text-3xl">
                 <p>Avancer : W</p>
