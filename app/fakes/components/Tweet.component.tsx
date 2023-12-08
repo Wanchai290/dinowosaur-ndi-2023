7b57e25bbc486e24155f9acb9df2ad663e7c422a
tfourcaudot <tom.fourcaudot@gmail.com>
2023-12-08 07:00:16 +0100

    FEAT: Display multiple tweets

--- app/fakes/components/Tweet.component.tsx
+++ app/fakes/components/Tweet.component.tsx
@@ -1,7 +1,27 @@
-export default function TweetComponent() {
+'use client';
+import {TweetModel} from "@/app/fakes/models/tweet.model";
+import {Avatar, Card} from "flowbite-react";
+import {useState} from "react";
+
+interface Props {
+    tweet: TweetModel
+}
+export default function TweetComponent({tweet}: Readonly<Props>) {
+    const [explainMode, setExplainMode] = useState(false);
+
     return(
-        <div>
-            <p>TweetComponent Work</p>
+        <div className="p-3">
+            <Card onClick={() => setExplainMode(!explainMode)}
+            className={explainMode
+              ? "dark:bg-gradient-to-b from-emerald-700 from-0% to-slate-800 to-60% cursor-pointer"
+            : "dark:bg-gradient-to-b from-blue-400 from-0% to-slate-800 to-60% cursor-pointer"}>
+                <Avatar img={tweet.pdpLink} rounded>
+                    <div className="space-y-1 font-medium dark:text-white">
+                        <div className="text-2xl ">{tweet.username}</div>
+                    </div>
+                </Avatar>
+                <p className="text-xl">{explainMode ? "Explication : " + tweet.explenation : tweet.tweetText}</p>
+            </Card>
         </div>
     )
 }
--- app/fakes/page.tsx
+++ app/fakes/page.tsx
@@ -1,7 +1,15 @@
+import TweetComponent from "@/app/fakes/components/Tweet.component";
+import {tweetsData} from "@/app/fakes/assets/data";
+
+
 export default function Page() {
     return(
-        <div>
-            <p>Tweet Page works</p>
+        <div className="grid grid-cols-2 gap-4 h-screen">
+            {
+                tweetsData.map((singleTweet) => (
+                    <TweetComponent tweet={singleTweet}/>
+                ))
+            }
         </div>
     )
 }
